b0e09aea4d8ea6e634820037f6ffa6ae6ae33293
ww-4 <andrey00600@gmail.com>
2025-09-10 17:29:25 +0300

    test

--- flutter_application_test/lib/main.dart
+++ flutter_application_test/lib/main.dart
@@ -1,20 +1,127 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
+  runApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: Padding(
+          padding: const EdgeInsets.all(12),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                height: 90,
+                alignment: Alignment.center,
+                decoration: const BoxDecoration(
+                  color: Color(0xFF7A0012),
+                  border: Border.fromBorderSide(
+                    BorderSide(color: Colors.black, width: 4),
+                  ),
+                ),
+                child: const Text(
+                  'TEXT',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+              const SizedBox(height: 16),
+              SizedBox(
+                height: 240,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: const [
+                    Expanded(
+                      flex: 2,
+                      child: DecoratedBox(
+                        decoration: BoxDecoration(
+                          color: Color(0xFF7A0012),
+                          border: Border.fromBorderSide(
+                            BorderSide(color: Colors.black, width: 4),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 16),
+                    Expanded(
+                      flex: 6,
+                      child: DecoratedBox(
+                        decoration: BoxDecoration(
+                          color: Color(0xFF7A0012),
+                          border: Border.fromBorderSide(
+                            BorderSide(color: Colors.black, width: 4),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 16),
+                    Expanded(
+                      flex: 2,
+                      child: DecoratedBox(
+                        decoration: BoxDecoration(
+                          color: Color(0xFF7A0012),
+                          border: Border.fromBorderSide(
+                            BorderSide(color: Colors.black, width: 4),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 16),
+              Container(
+                height: 90,
+                decoration: const BoxDecoration(
+                  color: Color(0xFF7A0012),
+                  border: Border.fromBorderSide(
+                    BorderSide(color: Colors.black, width: 4),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 16),
+              SizedBox(
+                height: 90,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: const [
+                    Expanded(
+                      child: DecoratedBox(
+                        decoration: BoxDecoration(
+                          color: Color(0xFF7A0012),
+                          border: Border.fromBorderSide(
+                            BorderSide(color: Colors.black, width: 4),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 16),
+                    Expanded(
+                      child: DecoratedBox(
+                        decoration: BoxDecoration(
+                          color: Color(0xFF7A0012),
+                          border: Border.fromBorderSide(
+                            BorderSide(color: Colors.black, width: 4),
+                          ),
+                        ),
+                      ),
+                    ),
+                    SizedBox(width: 16),
+                    Expanded(
+                      child: DecoratedBox(
+                        decoration: BoxDecoration(
+                          color: Color(0xFF7A0012),
+                          border: Border.fromBorderSide(
+                            BorderSide(color: Colors.black, width: 4),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-    );
-  }
+    ),
+  );
 }
